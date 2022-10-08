48bfa018ecbbf94414f000ab7836835dc96f959a
Amadeo Valar <amadeo.valar@gmail.com>
2022-10-08 21:21:18 +0200

    Starting a repository

---
+++ src/main/java/se1/aau/App.java
@@ -0,0 +1,13 @@
+package se1.aau;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
