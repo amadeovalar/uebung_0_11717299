6e404408a8e71b746d2be448a49f1901349f26e2
Amadeo Valar <amadeo.valar@gmail.com>
2022-10-09 15:25:33 +0200

    Test for normal cases successful

--- src/test/java/EinstiegsbeispielTest.java
+++ src/test/java/EinstiegsbeispielTest.java
@@ -5,13 +5,22 @@ import org.junit.Test;
 public class EinstiegsbeispielTest {
     
     @Test
-    public void resultIsCorrect()
+    public void resultWithPositiveNumbers()
     {
-        int[] num = {1,2,3};
+        int[] num = { 1, 2, 3 };
         int[] num2 = { 1, 2, 3 };
-    
-        assertEquals(14, Einstiegsbeispiel.getInnerProduct(num, num2)); 
+
+        assertEquals(14, Einstiegsbeispiel.getInnerProduct(num, num2));
     }
+    @Test
+    public void resultWithNegativeNumbers()
+    {
+        int[] num = {-1,2,3};
+        int[] num2 = { -1, -2, 3 };
+    
+        assertEquals(6, Einstiegsbeispiel.getInnerProduct(num, num2)); 
+    }
+
 
 
 }
